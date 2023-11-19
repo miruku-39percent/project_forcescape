03d22eba64f030aac94d5dadac99e9a7e4ce8a53
Thông Thái <46261937+nikethai@users.noreply.github.com>
2023-11-19 22:03:26 +0700

    Update uv.config.js

--- public/uv.config.js
+++ public/uv.config.js
@@ -1,7 +1,7 @@
 /*global Ultraviolet*/
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'http://localhost:8080/bare/',
+    bare: 'https://proxy.whyspace.click/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv.handler.js',
